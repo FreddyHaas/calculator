cbc56faf500d5a6a594f92797cb612e16e1016dd
Freddy Haas <email@frederik-haas.de>
2022-10-31 09:33:30 +0100

    Implement display screens 1 and 2

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-/* Prompt user for input */
-
 let numberOne = 0;
 let numberTwo = 0;
 let mathOperator = 0;
+
+/* Display inputs */
+const displayLarge = document.getElementById('displayLarge');
+const displaySmall = document.getElementById('displaySmall');
+
+function displayOperation () {
+    if (numberOne !== 0 && mathOperator === 0) {
+        displayLarge.textContent = `${numberOne}`;
+    }
+    if (numberOne !==0 && mathOperator !== 0) {
+        displaySmall.textContent = `${numberOne} ${mathOperator}`;
+        displaySmall.style.color = 'black';
+        displayLarge.textContent = `${numberOne}`;
+    }
+    if (numberOne !== 0 && mathOperator !== 0 && numberTwo !== 0) {
+        displayLarge.textContent = `${numberTwo}`;
+    }
+};
 
 /* Update and store input */
 
@@ -24,14 +40,16 @@ numbers.forEach((number) => {
     number.addEventListener ('click', () => {
         let currentNumber = number.textContent;
         storeNumber (currentNumber);
-        console.log(`NumberOne:${numberOne}; mathOperator:${mathOperator}; NumberTwo:${numberTwo}`)
+        console.log(`NumberOne:${numberOne}; mathOperator:${mathOperator}; NumberTwo:${numberTwo}`);
+        displayOperation ();
 })});
 
 const operators = document.querySelectorAll('#operator');
 operators.forEach((operator) => { 
     operator.addEventListener ('click', () => {
         mathOperator = operator.textContent;
-        console.log(`NumberOne:${numberOne}; mathOperator:${mathOperator}; NumberTwo:${numberTwo}`)
+        console.log(`NumberOne:${numberOne}; mathOperator:${mathOperator}; NumberTwo:${numberTwo}`);
+        displayOperation ();
     })
 })
 
@@ -70,19 +88,23 @@ function operate (numberOne, mathOperator, numberTwo) {
     }
 }
 
-/* Initiate mathematical operation upon clicking "=" */
+/* Initiate mathematical operation upon clicking "=" and show result*/
 
 const equal = document.querySelector ('#equal');
 equal.addEventListener ('click', () => {
+    displayLarge.textContent = `${operate(numberOne, mathOperator, numberTwo)}`;
+    displaySmall.textContent = `${numberOne} ${mathOperator} ${numberTwo}`;
     console.log (`Result: ${operate(numberOne, mathOperator, numberTwo)}`);
 });
 
-/* Clear function */
+/* Clear inputs and display */
 
 const clear = document.querySelector ('#clear');
 clear.addEventListener ('click', () => {
     numberOne = 0;
     numberTwo = 0; 
     mathOperator = 0;
+    displaySmall.style.color = 'white';
+    displayLarge.textContent = 0;
     console.log(`NumberOne:${numberOne}; mathOperator:${mathOperator}; NumberTwo:${numberTwo}`)
 });
